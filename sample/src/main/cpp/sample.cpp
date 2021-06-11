2fd8079f0f82b9978f6581bf22c356c50b5d1547
1831594078 <1831594078@qq.com>
2021-06-11 18:03:38 +0800

    add function call test case

--- sample/src/main/cpp/sample.cpp
+++ sample/src/main/cpp/sample.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <android/log.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include "adl.h"
 
@@ -42,7 +44,8 @@ static void adl_test_iterate(void) {
 }
 
 static void *
-adl_test_dlsym(const char *filename, const char *symbol, bool open_lib) {
+adl_test_dlsym(const char *filename, const char *symbol,
+               uintptr_t *out_sym_addr, bool open_lib) {
     if (open_lib) {
         void *handle = dlopen(filename, RTLD_NOW);
         LOG("--- dlopen(%s) : handle %p", filename, (uintptr_t) handle);
@@ -61,9 +64,10 @@ adl_test_dlsym(const char *filename, const char *symbol, bool open_lib) {
 
     // adlsym
     void *symbol_addr = adlsym(handle, symbol);
-    if (symbol_addr)
+    if (symbol_addr) {
         LOG(">>> adlsym(%s) : addr %p", symbol, (uintptr_t) symbol_addr);
-    else
+        *out_sym_addr = reinterpret_cast<uintptr_t>(symbol_addr);
+    } else
         LOGE("xxx adlsym(%s) not found", symbol);
 
     // adladdr
@@ -86,30 +90,40 @@ adl_test_dlsym(const char *filename, const char *symbol, bool open_lib) {
     return handle;
 }
 
+typedef int *(*gettimeofday_t)(struct timeval *tv, struct timezone *tz);
+
 static void adl_test(JNIEnv *env, jobject thiz) {
     (void) env;
     (void) thiz;
 
     adl_test_iterate();
 
+    void *handle = NULL;
+    uintptr_t symbol_addr = 0;
     // linker
     //inner symbol
-    adl_test_dlsym(BASENAME_LINKER, "__dl__Z14get_libdl_infoRK6soinfo", false);
+    adl_test_dlsym(BASENAME_LINKER, "__dl__Z14get_libdl_infoRK6soinfo", &symbol_addr, false);
     //export symbol
-    adl_test_dlsym(BASENAME_LINKER, "__loader_android_get_LD_LIBRARY_PATH", false);
+    adl_test_dlsym(BASENAME_LINKER, "__loader_android_get_LD_LIBRARY_PATH", &symbol_addr, false);
 
     // libc.so
     //inner symbol
-    adl_test_dlsym(BASENAME_LIBC, "__openat", false);
+    adl_test_dlsym(BASENAME_LIBC, "__openat", &symbol_addr, false);
     //export symbol
-    adl_test_dlsym(BASENAME_LIBC, "gettimeofday", false);
+    adl_test_dlsym(BASENAME_LIBC, "gettimeofday", &symbol_addr, false);
+    gettimeofday_t today = reinterpret_cast<gettimeofday_t>(symbol_addr);
+    struct timeval tv;
+    today(&tv, NULL);
+    struct tm *info = localtime(reinterpret_cast<const time_t *>(&tv));
+    LOG("Now is %s", asctime(info));
 
     // libc++.so
-    adl_test_dlsym(BASENAME_LIBCPP, "_ZNSt3__18valarrayImEC2Em", false);
-    adl_test_dlsym(PATHNAME_LIBCPP, "_ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEE3putEc", false);
+    adl_test_dlsym(BASENAME_LIBCPP, "_ZNSt3__18valarrayImEC2Em", &symbol_addr, false);
+    adl_test_dlsym(PATHNAME_LIBCPP, "_ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEE3putEc",
+                   &symbol_addr, false);
 
     //load elf file
-    void *handle = adl_test_dlsym(PATHNAME_LIBCURL, "Curl_open", true);
+    handle = adl_test_dlsym(PATHNAME_LIBCURL, "Curl_open", &symbol_addr, true);
     if (handle != NULL) {
         adlclose(handle);
     }
